2611c631919ca11508b803eec055015b052b314d
faun3 <antonioculcea@gmail.com>
2023-04-29 17:52:32 +0300

    make drop down drop down (with reusable function)

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("hello");
+
+const menuMaker = (menuContainerNode) => {
+  const expandButton = menuContainerNode.querySelector("#expandButton");
+
+  const menuContent = menuContainerNode.querySelector(".menuContent");
+
+  expandButton.addEventListener("click", () => {
+    menuContent.classList.toggle("hidden");
+  });
+};
+
+menuMaker(document.querySelector(".menuContainer"));
